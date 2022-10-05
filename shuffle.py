1361f9cdb57e6bdf3a7951b32a3cf565fa736609
DEBATEAPP <115107062+DEBATEAPP@users.noreply.github.com>
2022-10-05 16:33:10 -0600

    Add files via upload

---
+++ shuffle.py
@@ -0,0 +1,10 @@
+import random
+
+array = ['Listen','Speak','Speak','Opinions','Secure','Knowledge','Private','Connect','Communicating','Debate','Change','Discussion','Discourse','Argumentation','Vocalize','Reflect','Believe','Concentrate','Learning','Improve','Strengthen','Advise','Information','Debate','Reality','Honesty','Accountability','Agree','Disagree','Individuality']
+
+for x in range():
+    random.shuffle(array)
+    for i in range(14):
+        print(array[i], end=" ")
+        i+1
+    print('\n');
